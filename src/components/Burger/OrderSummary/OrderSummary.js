c2ee89722177d528860e44fd4a911279f687dca2
nirfuchs\fuchs <nirfuchs@hotmail.com>
2019-08-01 20:46:28 +0300

    Toolbar

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -4,27 +4,27 @@ import Aux from '../../../hoc/Auxiliary';
 import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
-    let ingredientsSummary = [];
-    for (let key in props.ingredients) {
-        ingredientsSummary.push(
-            <li key={key}>
-                <span style={{ textTransform: 'capitalize' }}>{key}</span>: {props.ingredients[key]}
-            </li>);
-    }
+  let ingredientsSummary = [];
+  for (let key in props.ingredients) {
+    ingredientsSummary.push(
+      <li key={key}>
+        <span style={{ textTransform: 'capitalize' }}>{key}</span>: {props.ingredients[key]}
+      </li>);
+  }
 
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientsSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.orderCancel}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.orderContinue}>CONTINUE</Button>
-        </Aux>
-    );
+  return (
+    <Aux>
+      <h3>Your Order</h3>
+      <p>A delicious burger with the following ingredients:</p>
+      <ul>
+        {ingredientsSummary}
+      </ul>
+      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+      <p>Continue to Checkout?</p>
+      <Button btnType="Danger" clicked={props.orderCancel}>CANCEL</Button>
+      <Button btnType="Success" clicked={props.orderContinue}>CONTINUE</Button>
+    </Aux>
+  );
 }
 
 export default orderSummary;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import Aux from '../../hoc/Auxiliary';
 
+import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 
 const layout = (props) => (
   <Aux>
-    <div>Toolbar, SideDrawer, Backdrop</div>
+    {/* <div>Toolbar, SideDrawer, Backdrop</div> */}
+    <Toolbar />
     <main className={classes.Content}>
       {props.children}
     </main>

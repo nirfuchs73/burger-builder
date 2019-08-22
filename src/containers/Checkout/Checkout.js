e18155001fb21c50dee02e22f44a3455464fe2d1
nirfuchs\fuchs <nirfuchs@hotmail.com>
2019-08-22 15:13:11 +0300

    Pass query parameters

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -3,12 +3,16 @@ import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSumm
 
 class Checkout extends Component {
   state = {
-    ingredients: {
-      salad: 1,
-      meat: 1,
-      bacon: 1,
-      cheese: 1,
+    ingredients: null,
+  }
+
+  componentDidMount() {
+    const query = new URLSearchParams(this.props.location.search);
+    const ingredients = {};
+    for (let param of query.entries()) {
+      ingredients[param[0]] = +param[1];
     }
+    this.setState({ ingredients: ingredients });
   }
 
   checkoutCancelledHandler = () => {

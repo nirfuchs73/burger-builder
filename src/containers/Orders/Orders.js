567173be4646698e9200d85028194a475a5681df
nirfuchs\fuchs <nirfuchs@hotmail.com>
2019-09-01 13:35:24 +0300

    Orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,16 +1,49 @@
 import React, { Component } from 'react';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 import Order from '../../components/Order/Order/Order';
 
 class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true,
+  }
+  componentDidMount() {
+    axios.get('/orders.json')
+      .then(res => {
+        const orders = [];
+        for (let key in res.data) {
+          orders.push({
+            ...res.data[key],
+            id: key,
+          });
+        }
+        // console.log(orders);
+
+        this.setState({ orders: orders })
+      })
+      .catch(err => {
+        console.log(err);
+      })
+      .finally(() => {
+        this.setState({ loading: false })
+      });
+  }
+
   render() {
+    const orders = this.state.orders.map(order => {
+      return <Order
+        key={order.id}
+        ingredients={order.ingredients}
+        price={+order.price} />;
+    })
     return (
       <div>
-        <Order />
-        <Order />
+        {orders}
       </div>
     );
   }
 }
 
-export default Orders
+export default withErrorHandler(Orders, axios);

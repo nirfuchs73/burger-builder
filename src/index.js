d690d1db8a2f9621835d45f9d0c232cd7a1d90de
nirfuchs\fuchs <nirfuchs@hotmail.com>
2019-09-04 11:40:31 +0300

    redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import reducer from './store/reducer';
+
+const store = createStore(reducer);
+store.subscribe(() => {
+  console.log('[Subscription]', store.getState());
+});
 
 const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
---
+++ src/store/reducer.js
@@ -0,0 +1,22 @@
+import * as actionTypes from './actions';
+
+const initialState = {
+  ingredients: null,
+  totalPrice: 4,
+}
+
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.ADD_INGREDIENT:
+      return Object.assign({}, state, { ingredients: null });
+
+    case actionTypes.REMOVE_INGREDIENT:
+      return Object.assign({}, state, { ingredients: null });
+
+    default:
+      return state;
+  }
+}
+
+export default reducer;

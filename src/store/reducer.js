0ddacadbfcf63ca6596f27a25c3e40dd1def25d3
nirfuchs\fuchs <nirfuchs@hotmail.com>
2019-09-04 13:26:46 +0300

    totalPrice

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -9,26 +9,34 @@ const initialState = {
   },
   totalPrice: 4,
 }
-
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.3,
+  bacon: 0.7,
+}
 
 const reducer = (state = initialState, action) => {
   let ingredients;
+  let totalPrice;
   switch (action.type) {
     case actionTypes.ADD_INGREDIENT:
       ingredients = {
         ...state.ingredients,
         [action.ingredient]: state.ingredients[action.ingredient] + 1,
       }
+      totalPrice = state.totalPrice + INGREDIENT_PRICES[action.ingredient];
 
-      return Object.assign({}, state, { ingredients: ingredients });
+      return Object.assign({}, state, { ingredients: ingredients, totalPrice: totalPrice });
 
     case actionTypes.REMOVE_INGREDIENT:
       ingredients = {
         ...state.ingredients,
         [action.ingredient]: state.ingredients[action.ingredient] - 1,
       }
+      totalPrice = state.totalPrice - INGREDIENT_PRICES[action.ingredient];
 
-      return Object.assign({}, state, { ingredients: ingredients });
+      return Object.assign({}, state, { ingredients: ingredients, totalPrice: totalPrice });
 
     default:
       return state;
